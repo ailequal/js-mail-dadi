dfb1a03dd67f296aa51e5990feb399abad390a94
ailequal <37016865+ailequal@users.noreply.github.com>
2020-01-07 15:58:32 +0100

    Added dice game working

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,1 +1,22 @@
-console.log('hello world');
+// user throw a dice
+var userDice = Math.floor(Math.random() * 6) + 1;
+// show result
+console.log('userDice is');
+console.log(userDice);
+
+
+// bot throw a dice
+var botDice = Math.floor(Math.random() * 6) + 1;
+// show result
+console.log('botDice is');
+console.log(botDice);
+
+
+// which one is the biggest
+if (userDice > botDice) {
+  console.log('The user win');
+} else if (userDice < botDice) {
+  console.log('The bot win');
+} else {
+  console.log('No winner');
+}
